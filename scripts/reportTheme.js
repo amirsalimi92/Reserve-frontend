8d62474a20149b7d8229337f6badc804885cdce6
amir <amir.salimi1810@gmail.com>
2022-12-27 20:44:22 +0100

    endOfReport

--- scripts/reportTheme.js
+++ scripts/reportTheme.js
@@ -9,33 +9,33 @@ if (themeModeFirst === "night") {
   localStorage.setItem("themeModeReserve", "day");
 }
 
+// run the function
+
 themeSelector.addEventListener("click", () => {
   nightTheme();
 });
 
 function nightTheme() {
+  // navbar
   document.querySelector(".navbar").classList.toggle("nightNavbar");
+  document.querySelector(".navMenu").classList.toggle("navMenuNight");
   document.querySelectorAll(".dropdown").forEach((e) => {
     e.classList.toggle("nightDropdown");
   });
-  document.querySelector(".content").classList.toggle("night");
-  document.querySelector(".profileName").classList.toggle("profileLeftNight");
-  document.querySelector(".profileFamily").classList.toggle("profileLeftNight");
-  document
-    .querySelector(".profilePhoto")
-    .classList.toggle("profileTopLeftNight");
-  document.querySelectorAll("input").forEach((e) => {
-    e.classList.toggle("profileInputNight");
-  });
-  document.querySelectorAll("select").forEach((e) => {
-    e.classList.toggle("profileSelectNight");
-  });
-  document
-    .querySelector(".profileButton > button")
-    .classList.toggle("profileButtonNight");
 
-  document.querySelector(".navMenu").classList.toggle("navMenuNight");
+  // report
+  document.querySelector(".reportBase").classList.toggle("reportBaseNight");
+  document
+    .querySelector(".reportButton > button")
+    .classList.toggle("reportButtonNight");
+  document
+    .querySelector(".reportSender > select")
+    .classList.toggle("reportSenderNight");
+  document
+    .querySelector(".reportSender > textarea")
+    .classList.toggle("reportSenderNight");
 
+  // set the theme
   if (localStorage.getItem("themeModeReserve") == "night") {
     localStorage.setItem("themeModeReserve", "day");
   } else if (localStorage.getItem("themeModeReserve") == "day") {
