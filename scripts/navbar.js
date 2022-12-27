712dc4846147961989994a2552c36aa0c5efe072
amir <amir.salimi1810@gmail.com>
2022-12-27 01:54:50 +0100

    endOfNavbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -6,7 +6,7 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll(".navMenuItemLink").forEach((n) =>
+document.querySelectorAll("a[href]").forEach((n) =>
   n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
